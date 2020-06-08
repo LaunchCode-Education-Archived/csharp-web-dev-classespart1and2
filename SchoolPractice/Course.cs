e7584c10c8cf7290f8c5b7f12851296899326603
William Morrison <williamchrismorrison@gmail.com>
2020-06-08 12:20:59 -0700

    finalized

--- SchoolPractice/Course.cs
+++ SchoolPractice/Course.cs
@@ -6,35 +6,30 @@ namespace SchoolPractice
 {
     public class Course
     {
-        private int level; //100-899, i.e. MATH 323
         private List<Student> Roster;
         public string Name { get; set; }
         public int NumberOfCredits { get; set; }
 
-        public Course(int level, string name, int numberOfCredits)
+        public Course(string name, int numberOfCredits)
         {
-            this.level = level;
+
             Name = name;
             NumberOfCredits = numberOfCredits;
         }
 
-        public int Level
+        public override bool Equals(object obj)
         {
-            get
-            {
-                return level;
-            }
-
-            set
-            {
-                if (value > 899 || value < 100 || value % 1 != 0)
-                {
-                    throw new ArgumentException("Invalid course number");
-                }
-
-                level = value; 
-            }
+            return base.Equals(obj);
         }
 
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return Name + " (Credits: " + NumberOfCredits + ")";
+        }
     }
 }
--- SchoolPractice/Student.cs
+++ SchoolPractice/Student.cs
@@ -7,17 +7,96 @@ namespace SchoolPractice
 {
     public class Student
     {
+
+        private static int nextStudentId = 1;
         public string Name { get; set; }
-        public int StudentId { get; set; }
+        private readonly int studentId;
         public int NumberOfCredits { get; set; }
         public double Gpa { get; set; }
 
-        public Student(string name, int numberOfCredits, double gpa)
+        public Student(string name, int sId, int numberOfCredits, double gpa)
         {
             Name = name;
-            StudentId = 13;
+            studentId = sId;
             NumberOfCredits = numberOfCredits;
             Gpa = gpa;
+        }
+
+        public Student(string name, int sId)
+        {
+            Name = name;
+            studentId = sId;
+            NumberOfCredits = 0;
+            Gpa = 0.0;
+        }
+
+        public Student(string name)
+        {
+            Name = name;
+            studentId = nextStudentId;
+            nextStudentId++;
+            NumberOfCredits = 0;
+            Gpa = 0.0;
+        }
+
+        public string StudentInfo()
+        {
+            return (Name + " has a GPA of: " + Gpa);
+        }
+
+        public void AddGrade(int courseCredits, double grade)
+        {
+            // Update the appropriate fields: NumberOfCredits, Gpa
+            float totalQualityScore = (float)Gpa * NumberOfCredits;
+            totalQualityScore += (float)grade * courseCredits;
+            NumberOfCredits += courseCredits;
+            Gpa = totalQualityScore / NumberOfCredits;
+        }
+
+        public string GetGradeLevel()
+        {
+            if (NumberOfCredits <= 29)
+            {
+                return "Freshman";
+            }
+            else if (NumberOfCredits <= 59 && NumberOfCredits >= 30)
+            {
+                return "Sophmore";
+            }
+            else if (NumberOfCredits <= 89 && NumberOfCredits >= 60)
+            {
+                return "Junior";
+            }
+            else
+            {
+                return "Senior";
+            }
+        }
+
+        public override bool Equals(object toBeCompared)
+        {
+            if (toBeCompared == this)
+            {
+                return true;
+            }
+
+            if (toBeCompared == null)
+            {
+                return false;
+            }
+
+            if (toBeCompared.GetType() != this.GetType())
+            {
+                return false;
+            }
+
+            Student s = toBeCompared as Student;
+            return s.studentId == studentId;
+        }
+
+        public override string ToString()
+        {
+            return Name + " (Credits: " + NumberOfCredits + ", GPA: " + Gpa + ")";
         }
     }
 
